7098f2985341ff05f17be38b2b4ed61d2c0827f1
YuanLicc <yl123456789lxp@163.com>
2025-03-15 01:17:59 +0800

    FindKthLargest

---
+++ algorithm/src/main/java/com/yl/learn/algorithm/leetcode/FindKthLargest.java
@@ -0,0 +1,41 @@
+package com.yl.learn.algorithm.leetcode;
+
+public class FindKthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        buildMaxHeap(nums, nums.length);
+        for(int i = nums.length - 1; i >= nums.length - k + 1; i--) {
+            swap(nums, 0, i);
+            heap(nums, 0, i);
+        }
+        return nums[0];
+    }
+
+    private void buildMaxHeap(int[] nums, int heapSize) {
+        for(int i = heapSize / 2 - 1; i >= 0; i--) {
+            heap(nums, i, heapSize);
+        }
+    }
+
+    private void heap(int[] nums, int node, int heapSize) {
+        int leftNode = node * 2 + 1;
+        int rightNode = node * 2 + 2;
+        int maxNode = node;
+        if(leftNode < heapSize && nums[leftNode] > nums[maxNode]) {
+            maxNode = leftNode;
+        }
+        if(rightNode < heapSize && nums[rightNode] > nums[maxNode]) {
+            maxNode = rightNode;
+        }
+
+        if(maxNode != node) {
+            swap(nums, node, maxNode);
+            heap(nums, maxNode, heapSize); // 交换后平衡后续节点
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
