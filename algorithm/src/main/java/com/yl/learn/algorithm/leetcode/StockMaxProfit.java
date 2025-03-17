2dd8d1a7a59f2f88a79d1246acb5c28745ef17c8
YuanLicc <yl123456789lxp@163.com>
2025-03-17 15:26:30 +0800

    StockMaxProfit

---
+++ algorithm/src/main/java/com/yl/learn/algorithm/leetcode/StockMaxProfit.java
@@ -0,0 +1,26 @@
+package com.yl.learn.algorithm.leetcode;
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * 示例 1：
+ * 输入：[7,1,5,3,6,4]
+ * 输出：5
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ */
+public class StockMaxProfit {
+
+    public int maxProfit(int[] prices) {
+        int max = prices[prices.length - 1];
+        int maxSub = 0;
+        for(int i = prices.length - 2; i >= 0; i--) {
+            maxSub = Math.max(maxSub, max - prices[i]);
+            max = Math.max(max, prices[i]);
+        }
+        return maxSub;
+    }
+
+}
